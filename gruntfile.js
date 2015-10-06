635572b0dc718eabee9fbd5df739480c4f8cfd35
Dan Karlsson <dan.e.karlsson@jpl.nasa.gov>
2015-10-06 15:57:14 -0700

    Current master that is deployed to the server.

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function (grunt) {
     var jsFiles = ['app/js/**/*.js', 'src/**/*.js', '!app/vendor/'];
     grunt.initConfig({
-
         'bower-install-simple': {
             options: {
                 color: true,
@@ -23,7 +22,6 @@ module.exports = function (grunt) {
                            src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
                 }
             },
-
             directives: {
                 src: ['src/directives/*.js'],
                 dest: 'build/same.directives.js'
@@ -37,7 +35,6 @@ module.exports = function (grunt) {
                 dest: 'build/js/controllers.js'
             }
         },
-
         sass: {
             dist: {
                 options: {
@@ -61,9 +58,7 @@ module.exports = function (grunt) {
         },
         wiredep: {
             target: {
-                src: [
-                    'build/*.html'
-                ]
+                src: ['build/*.html']
             }
         },
         jshint: {
@@ -72,9 +67,19 @@ module.exports = function (grunt) {
             options: {
                 globalstrict: true,
                 globals: {
+                    jQuery: true,
+                    document: true,
                     angular: true,
                     window: true,
-                    console: true
+                    console: true,
+                    Firebase: true
+                }
+            }
+        },
+        uglify: {
+            build: {
+                files: {
+                    'build/js/same.min.js': ['app/js/app.js', 'app/js/controllers.js']
                 }
             }
         },
@@ -91,35 +96,19 @@ module.exports = function (grunt) {
         copy: {
             main: {
                 files: [
-
-                    {
-                        expand: true, src: '**', cwd: 'app/css/', dest: 'build/css/'
-                    },
-                    {
-                        expand: true, src: '**', cwd: 'app/img/', dest: 'build/img/'
-                    },
-                    {
-                        expand: true, src: '**', cwd: 'app/partials/', dest: 'build/partials/'
-                    },
-                    {
-                        expand: true, src: '**', cwd: 'app/partials/templates/', dest: 'build/partials/templates/'
-                    },
-                    {
-                        expand: true, src: '**', cwd: 'app/js/', dest: 'build/js/'
-                    },
-                    {
-                        src: 'app/index.html', cwd: '.', dest: 'build/index.html'
-                    }
+                    {expand: true, src: '**', cwd: 'app/css/', dest: 'build/css/'},
+                    {expand: true, src: '**', cwd: 'app/img/', dest: 'build/img/'},
+                    {expand: true, src: '**', cwd: 'app/partials/', dest: 'build/partials/'},
+                    {expand: true, src: '**', cwd: 'app/partials/templates/', dest: 'build/partials/templates/'},
+                    {expand: true, src: '**', cwd: 'app/js/', dest: 'build/js/'},
+                    {src: 'app/index.html', cwd: '.', dest: 'build/index.html'}
                 ]
             },
             bower: {
                 files: [
-                    {
-                        expand: true, src: '**', cwd: 'app/bower_components/', dest: 'build/bower_components/'
-                    }
+                    {expand: true, src: '**', cwd: 'app/bower_components/', dest: 'build/bower_components/'}
                 ]
             }
-
         }, clean: {
             all: ['app/bower_components', 'build/*'],
             bower: ['app/bower_components'],
@@ -132,23 +121,12 @@ module.exports = function (grunt) {
             copy: {
                 main: {
                     files: [
-                        {
-                            expand: true, src: '**', cwd: 'app/bower_components/', dest: 'deploy/bower_components/'
-                        },
-                        {
-                            expand: true, src: '**', cwd: 'app/css/', dest: 'deploy/css/'
-                        },
-                        {
-                            expand: true, src: '**', cwd: 'app/img/', dest: 'deploy/img/'
-                        },
-                        {
-                            expand: true, src: '**', cwd: 'app/partials/', dest: 'deploy/partials/'
-                        }, {
-                            expand: true, src: '**', cwd: 'app/js/', dest: 'deploy/js/'
-                        },
-                        {
-                            src: 'app/index.html', cwd: '.', dest: 'deploy/index.html'
-                        }
+                        {expand: true, src: '**', cwd: 'app/bower_components/', dest: 'deploy/bower_components/'},
+                        {expand: true, src: '**', cwd: 'app/css/', dest: 'deploy/css/'},
+                        {expand: true, src: '**', cwd: 'app/img/', dest: 'deploy/img/'},
+                        {expand: true, src: '**', cwd: 'app/partials/', dest: 'deploy/partials/'},
+                        {expand: true, src: '**', cwd: 'app/js/', dest: 'deploy/js/'},
+                        {src: 'app/index.html', cwd: '.', dest: 'deploy/index.html'}
                     ]
                 }
             }
@@ -167,21 +145,20 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-bower-install-simple');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('cleanBower', ['clean:bower']);
-    grunt.registerTask('cleanAll', ['clean:all']);
-    grunt.registerTask('cleanBuild', ['clean:build']);
-    grunt.registerTask('install', ['bower-install-simple']);
-    grunt.registerTask('compile', ['sass', 'copy:main', 'copy:bower']);
-    grunt.registerTask('lint', ['jshint:beforeconcat']);
-    grunt.registerTask('wire', ['wiredep']);
+    grunt.registerTask('cleanBower',    ['clean:bower']);
+    grunt.registerTask('cleanAll',      ['clean:all']);
+    grunt.registerTask('cleanBuild',    ['clean:build']);
+    grunt.registerTask('install',       ['bower-install-simple']);
+    grunt.registerTask('compile',       ['sass', 'copy:main', 'copy:bower']);
+    grunt.registerTask('lint',          ['jshint:beforeconcat']);
+    grunt.registerTask('wire',          ['wiredep']);
 
     // Registers tasks to be executed when the task name is called.
-    grunt.registerTask('build', ['install', 'concat', 'copy:main', 'wire']);
-    grunt.registerTask('dev', ['jshint', 'connect', 'watch']);
-    grunt.registerTask('default', ['install', 'jshint']);
-    grunt.registerTask('server', ['build', 'connect', 'watch']);
-    grunt.registerTask('deploy', ['install', 'compile', 'build', 'connect'])
-
-
+    grunt.registerTask('build',     ['install', 'jshint', 'concat', 'copy:main', 'wire']);
+    grunt.registerTask('dev',       ['jshint', 'connect', 'watch']);
+    grunt.registerTask('default',   ['install', 'jshint']);
+    grunt.registerTask('server',    ['build', 'connect', 'watch']);
+    grunt.registerTask('deploy',    ['install', 'compile', 'build', 'connect'])
 }; // Wrapper function
