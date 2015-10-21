5fedc3370645cac53500faf4562a87089c5f7d68
Dan Karlsson <dan.e.karlsson@jpl.nasa.gov>
2015-10-20 21:56:54 -0700

    Added about me for the people who sent me the information. Will be adding pictures next.

---
+++ app/js/controllers/resource.controller.js
@@ -0,0 +1,12 @@
+"use strict";
+
+angular.module('same').controller('ResourceCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    $http.get('https:// same-webapp.firebaseapp.com/lib/json/images.json').then(function (data) {
+            $scope.images = data;
+        },
+        (function (response) {
+            // Alert of failure
+        }));
+
+}]);
